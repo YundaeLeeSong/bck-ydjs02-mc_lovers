7eb49f324955c6a5a4f404ba09439e2aaad891cd
YundaeLeeSong <ydleesong@gmail.com>
2026-01-25 22:49:00 -0500

    revert(build): consider sillicon mac and any ARM architectured CPUs

---
+++ gradle/jpackage.gradle.kts
@@ -0,0 +1,3 @@
+// This file is deprecated.
+// The JPackage logic has been moved to 'app/build.gradle.kts' to simplify the build structure.
+// You can safely delete this file if it is no longer referenced.
